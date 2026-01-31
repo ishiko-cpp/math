39edfdfce78a417983d7ed02456b531de3af867d
Xavier Leclercq <xavier@leclercq.dev>
2026-01-31 20:27:24 +0000

    Updates

---
+++ numbers/tests/src/main.cpp
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: 2000-2026 Xavier Leclercq
+// SPDX-License-Identifier: BSL-1.0
+
+#include <Ishiko/TestFramework/Core.hpp>
+#include <exception>
+
+using namespace Ishiko;
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        TestHarness::CommandLineSpecification command_line_spec;
+        Configuration configuration = command_line_spec.createDefaultConfiguration();
+        CommandLineParser::parse(command_line_spec, argc, argv, configuration);
+
+        TestHarness the_test_harness("Ishiko/C++ Numbers Library Tests", configuration);
+
+        TestSequence& theTests = the_test_harness.tests();
+
+        return the_test_harness.run();
+    }
+    catch (const std::exception& e)
+    {
+        return TestApplicationReturnCode::exception;
+    }
+    catch (...)
+    {
+        return TestApplicationReturnCode::exception;
+    }
+}
