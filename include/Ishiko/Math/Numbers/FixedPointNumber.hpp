a130dba66f965ead9e3554e5e3a73396b3e409bf
Xavier Leclercq <xavier@leclercq.dev>
2026-01-31 23:40:53 +0000

    Update FixedPointNumber.hpp

--- include/Ishiko/Math/Numbers/FixedPointNumber.hpp
+++ include/Ishiko/Math/Numbers/FixedPointNumber.hpp
@@ -9,7 +9,29 @@ namespace Ishiko
 	template<size_t precision, size_t scale>
 	class FixedPointNumber
 	{
+		static_assert(scale <= precision, "Scale cannot exceed precision");
+
+    public:
+        FixedPointNumber();
+
+    private:
+        // Helper to select appropriate integer type based on precision
+        using storage_type =
+            std::conditional_t<
+                (precision <= std::numeric_limits<int32_t>::digits10),
+                int32_t,
+                std::conditional_t<(precision <= std::numeric_limits<int64_t>::digits10), int64_t, void>
+            >;
+        static_assert(!std::is_void<storage_type>::value, "Precision exceeds maximum supported value (18 digits)");
+
+        storage_type m_value;
 	};
+}
+
+template<size_t precision, size_t scale>
+Ishiko::FixedPointNumber<precision, scale>::FixedPointNumber()
+    : m_value(0)
+{
 }
 
 #endif
